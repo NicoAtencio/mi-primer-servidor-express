869f70e3dfed47a9e9a02f34b08fff176527fb19
Hector Nicolas Atencio <nicolasatencio90@gmail.com>
2023-05-24 16:18:16 -0300

    Creacion de verbos para routers estandar

--- routers/estandar.js
+++ routers/estandar.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { usuarios } = require('../datos/usuarios.js');
 const {estandar} = require('../datos/usuarios.js').usuarios;
 
 const routerEstandar = express.Router();
@@ -10,7 +11,6 @@ const ordenarPorCantidadDeSeguidores = (arr) => {
 
 routerEstandar.get('/', (req,res) => {
     if(req.query.ordenar === 'seguidores') return res.send(ordenarPorCantidadDeSeguidores(estandar));
-    
     res.send(estandar)
 });
 
@@ -19,6 +19,41 @@ routerEstandar.get('/:id', (req,res) => {
     const resultado = estandar.filter(usuario => usuario.id == id);
     if(resultado.length === 0) return res.status(204).send(`El identificardor ${id} no esta asociado a un usuario.`);
     res.send(resultado)
+});
+
+routerEstandar.post('/', (req,res) => {
+    const usuarioNuevo = req.body;
+    estandar.push(usuarioNuevo);
+    res.send(estandar);
+});
+
+routerEstandar.put('/:id', (req,res) => {
+    const modificarCompletamente = req.body;
+    const id = req.params.id;
+    const indice = estandar.findIndex(usuario => usuario.id == id);
+    if(indice >= 0) estandar[indice] = modificarCompletamente;
+    else return res.status(404).send(`No existe el usuario con el identificador ${id}`)
+    res.send(estandar);
+});
+
+routerEstandar.patch('/:id', (req,res) => {
+    const editarUsuario = req.body;
+    const id = req.params.id;
+    const indice = estandar.findIndex(usuario => usuario.id == id);
+    if(indice >= 0) Object.assign(estandar[indice], editarUsuario)
+    else return res.status(404).send(`No existe el usuario con el identificador ${id}`);
+    res.send(estandar)
+});
+
+routerEstandar.delete('/:id', (req,res) => {
+    const id = req.params.id;
+    const indice = estandar.findIndex(usuario => usuario.id == id);
+    if(indice >= 0){
+        estandar.splice(indice,1);
+        res.send(estandar)
+    }else{
+        res.status(204).send(`No existe el usuario con el identificador ${id}`)
+    }
 })
 
 
